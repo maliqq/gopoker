538d245843e6db1d9db6d60c763fd482148d255b
Malik Baktiyarov <malikbakt@gmail.com>
2013-07-08 20:51:12 +0400

    play: play states

--- play/context/play.go
+++ play/context/play.go
@@ -11,14 +11,26 @@ import (
 	"gopoker/util/console"
 )
 
+type State string
+
+const (
+	Active = "active"
+	Waiting = "waiting"
+	Paused = "paused"
+	Closed = "closed"
+)
+
 type Play struct {
+	State
 	*model.Deal
 	*model.Table
 	*model.Game
 
 	*protocol.Broadcast
 	Receive  chan *protocol.Message
-	NextTurn chan *protocol.Message
+	Betting chan *protocol.Message
+	Discarding chan *protocol.Message
+
 	NextDeal chan int
 }
 
@@ -28,17 +40,43 @@ func (play *Play) String() string {
 
 func NewPlay(game *model.Game, table *model.Table) *Play {
 	play := &Play{
-		Game:      game,
-		Table:     table,
-		Broadcast: protocol.NewBroadcast(),
-		Receive:   make(chan *protocol.Message),
-		NextTurn:  make(chan *protocol.Message),
-		NextDeal:  make(chan int),
+		State:      Waiting,
+		Game:       game,
+		Table:      table,
+		Broadcast:  protocol.NewBroadcast(),
+		Receive:    make(chan *protocol.Message),
+		Betting:    make(chan *protocol.Message),
+		NextDeal:   make(chan int),
+	}
+	
+	if game.Options.Discards {
+		play.Discarding = make(chan *protocol.Message)
 	}
 
-	go play.receive()
+	play.receive()
 
 	return play
+}
+
+func (play *Play) Start() {
+	play.receive()
+}
+
+func (play *Play) Close() {
+	play.State = Closed
+}
+
+func (play *Play) Pause() {
+	play.State = Paused
+}
+
+func (play *Play) Resume() {
+	play.State = Active
+}
+
+func (play *Play) nextDeal() {
+	play.Deal = model.NewDeal()
+	play.State = Active
 }
 
 func (play *Play) receive() {
@@ -48,12 +86,28 @@ func (play *Play) receive() {
 			log.Printf(console.Color(console.YELLOW, msg.String()))
 
 			switch msg.Payload.(type) {
+			case protocol.JoinPlayer:
+				join := msg.Payload.(protocol.JoinPlayer)
+				play.Table.AddPlayer(join.Player, join.Pos, join.Amount)
+
+			case protocol.LeaveTable:
+				leave := msg.Payload.(protocol.LeaveTable)
+				play.Table.RemovePlayer(leave.Player)
+
+			case protocol.ChangeSeatState:
+				change := msg.Payload.(protocol.ChangeSeatState)
+				seat := play.Table.Seat(change.Pos)
+				seat.State = change.State
+			
+			case protocol.DiscardCards:
+				play.Discarding <- msg
+
 			case protocol.AddBet:
-				play.NextTurn <- msg
+				play.Betting <- msg
 			}
 
 		case <-play.NextDeal:
-			log.Println("starting new deal")
+			play.nextDeal()
 		}
 	}
 }
