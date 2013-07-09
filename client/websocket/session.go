e2e98ea533e5d90186f325ecc9765a26007e32f3
Malik Baktiyarov <malik@baktiyarov.com>
2013-07-10 01:09:19 +0400

    gofmt

--- client/websocket/session.go
+++ client/websocket/session.go
@@ -1,51 +1,51 @@
 package websocket
 
 import (
-  "code.google.com/p/go.net/websocket"
+	"code.google.com/p/go.net/websocket"
 )
 
 type Session struct {
-  Connection *websocket.Conn
-  Receive chan string
-  Send *chan string
+	Connection *websocket.Conn
+	Receive    chan string
+	Send       *chan string
 }
 
 func NewSession(connection *websocket.Conn) *Session {
-  return &Session{
-    Connection: connection,
-    Receive: make(chan string),
-  }
+	return &Session{
+		Connection: connection,
+		Receive:    make(chan string),
+	}
 }
 
 func Handler(connection *websocket.Conn) {
-  session := NewSession(connection)
-  
-  go session.receive()
-  session.send()
+	session := NewSession(connection)
+
+	go session.receive()
+	session.send()
 }
 
 func (session *Session) receive() {
-  for {
-    var message string
-  
-    err := websocket.Message.Receive(session.Connection, &message)
-    if err != nil {
-      break
-    }
-  
-    *session.Send <- message
-  }
-  
-  session.Connection.Close()
-} 
+	for {
+		var message string
+
+		err := websocket.Message.Receive(session.Connection, &message)
+		if err != nil {
+			break
+		}
+
+		*session.Send <- message
+	}
+
+	session.Connection.Close()
+}
 
 func (session *Session) send() {
-  for message := range session.Receive {
-    err := websocket.Message.Send(session.Connection, message)
-    if err != nil {
-      break
-    }
-  }
+	for message := range session.Receive {
+		err := websocket.Message.Send(session.Connection, message)
+		if err != nil {
+			break
+		}
+	}
 
-  session.Connection.Close()
+	session.Connection.Close()
 }
--- play/context/play.go
+++ play/context/play.go
@@ -7,9 +7,9 @@ import (
 
 import (
 	"gopoker/model"
+	"gopoker/play/command"
 	"gopoker/protocol"
 	"gopoker/util/console"
-	"gopoker/play/command"
 )
 
 type Play struct {
