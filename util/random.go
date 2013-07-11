f6fdb7cf23789f76523ba0ab8d261e460ef95e39
Malik Baktiyarov <malikbakt@gmail.com>
2013-07-11 17:10:12 +0400

    util: refactoring

---
+++ util/random.go
@@ -0,0 +1,16 @@
+package util
+
+import (
+	"fmt"
+	"os"
+)
+
+func RandomUuid() string {
+	f, _ := os.Open("/dev/urandom")
+	defer f.Close()
+
+	bytes := make([]byte, 16)
+	f.Read(bytes)
+
+	return fmt.Sprintf("%x-%x-%x-%x-%x", bytes[0:4], bytes[4:6], bytes[6:8], bytes[8:10], bytes[10:])
+}
