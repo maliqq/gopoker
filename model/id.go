d1296c7871b3930f928ec4f319d6be69977a60a8
Malik Baktiyarov <malikbakt@gmail.com>
2013-07-11 17:10:20 +0400

    util: refactoring

--- model/id.go
+++ model/id.go
@@ -1,22 +1,11 @@
 package model
 
 import (
-	"fmt"
-	"os"
+	"gopoker/util"
 )
 
 type Id string
 
-func random_uuid() string {
-	f, _ := os.Open("/dev/urandom")
-	defer f.Close()
-
-	bytes := make([]byte, 16)
-	f.Read(bytes)
-
-	return fmt.Sprintf("%x-%x-%x-%x-%x", bytes[0:4], bytes[4:6], bytes[6:8], bytes[8:10], bytes[10:])
-}
-
 func RandomId() Id {
-	return Id(random_uuid())
+	return Id(util.RandomUuid())
 }
