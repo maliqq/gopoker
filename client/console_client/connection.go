fcc0925e8da20446f9d133c4f98a30e4437c2c98
Malik Baktiyarov <malikbakt@gmail.com>
2013-07-17 20:55:58 +0400

    console_client implementation; cleanup gopoker-console/

---
+++ client/console_client/connection.go
@@ -0,0 +1,209 @@
+package console_client
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+import (
+	"gopoker/poker"
+	"gopoker/protocol"
+	"gopoker/util/console"
+
+	"gopoker/model"
+
+	"gopoker/play"
+)
+
+type Connection struct {
+	Play *play.Play
+}
+
+func (c *Connection) Close() error {
+	return nil
+}
+
+func (c *Connection) Receive(data interface{}) error {
+	return nil
+}
+
+func (c *Connection) Send(data interface{}) error {
+	return nil
+}
+
+func (c *Connection) Reply(msg *protocol.Message) {
+	
+}
+
+func (c *Connection) Process(msg *protocol.Message) {
+	log.Println(console.Color(console.GREEN, fmt.Sprintf("[receive] %s", msg)))
+
+	switch msg.Payload.(type) {
+	case protocol.RequireBet:
+
+		r := msg.Payload.(protocol.RequireBet)
+
+		fmt.Printf("%s\n", r)
+
+		var newBet *model.Bet
+
+		seat := c.Play.Table.Seat(r.Pos)
+
+		for newBet == nil {
+			newBet = readBet(r.Call, r.Call - seat.Bet)
+
+			err := newBet.Validate(seat, r.BetRange)
+			if err != nil {
+				fmt.Println(err.Error())
+				newBet = nil
+			}
+		}
+
+		if newBet != nil {
+			c.Reply(protocol.NewAddBet(r.Pos, newBet))
+		}
+
+	case protocol.RequireDiscard:
+
+		r := msg.Payload.(protocol.RequireDiscard)
+
+		seat := c.Play.Table.Seat(r.Pos)
+
+		fmt.Printf("Your cards: [%s]\n", c.Play.Deal.Pocket(seat.Player))
+
+		var cards *poker.Cards
+		for cards == nil {
+			cards = readCards()
+		}
+
+		c.Reply(protocol.NewDiscardCards(r.Pos, cards))
+
+	case protocol.DealCards:
+
+		payload := msg.Payload.(protocol.DealCards)
+
+		if payload.Type.IsBoard() {
+			fmt.Printf("Dealt %s %s\n", payload.Type, payload.Cards.ConsoleString())
+		} else {
+			fmt.Printf("Dealt %s %s to %d\n", payload.Type, payload.Cards.ConsoleString(), payload.Pos)
+		}
+
+	case protocol.MoveButton:
+
+		payload := msg.Payload.(protocol.MoveButton)
+
+		fmt.Printf("Button is %d\n", payload.Pos+1)
+
+	case protocol.AddBet:
+
+		payload := msg.Payload.(protocol.AddBet)
+
+		player := c.Play.Table.Player(payload.Pos)
+
+		fmt.Printf("Player %s: %s\n", player, payload.Bet)
+
+	case protocol.PotSummary:
+
+		payload := msg.Payload.(protocol.PotSummary)
+
+		fmt.Printf("Pot size: %.2f\nBoard: %s\n", payload.Amount, c.Play.Deal.Board.ConsoleString())
+
+	case protocol.ShowHand:
+
+		payload := msg.Payload.(protocol.ShowHand)
+
+		player := c.Play.Table.Player(payload.Pos)
+
+		fmt.Printf("Player %s has %s (%s)\n", player, payload.Cards.ConsoleString(), payload.Hand.HumanString())
+
+	case protocol.Winner:
+
+		payload := msg.Payload.(protocol.Winner)
+
+		fmt.Printf("Player %s won %.2f\n", payload.Player, payload.Amount)
+
+	case protocol.ChangeGame:
+
+		payload := msg.Payload.(protocol.ChangeGame)
+
+		fmt.Printf("Game changed to %s %s\n", payload.Type, payload.Limit)
+	}
+}
+
+func readLine() string {
+	fmt.Print(">>> ")
+	str, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	str = strings.TrimRight(str, "\n")
+	return str
+}
+
+func readBet(call float64, toCall float64) *model.Bet {
+	var b *model.Bet
+
+	for b == nil {
+		b = parseBet(call, toCall, readLine())
+	}
+
+	return b
+}
+
+func readCards() *poker.Cards {
+	var cards *poker.Cards
+	for cards == nil {
+		str := readLine()
+		var err error
+		cards, err = poker.ParseCards(str)
+		if err != nil {
+			fmt.Printf("error: %s\n", err)
+		}
+	}
+	return cards
+}
+
+func parseBet(call float64, toCall float64, str string) *model.Bet {
+	var b *model.Bet
+
+	switch str {
+	case "":
+		if toCall == 0. { // nothing to call
+			b = model.NewCheck()
+		} else {
+			b = model.NewCall(call)
+		}
+
+	case "fold":
+		b = model.NewFold()
+
+	case "check":
+		b = model.NewCheck()
+
+	case "call":
+		b = model.NewCall(call)
+
+	default:
+		parts := strings.Split(str, " ")
+
+		var amountString string
+		if len(parts) == 1 {
+			amountString = parts[0]
+		} else if len(parts) == 2 && parts[0] == "raise" {
+			amountString = parts[1]
+		}
+
+		if amountString != "" {
+			amount, err := strconv.ParseFloat(amountString, 64)
+
+			if err == nil {
+				b = model.NewRaise(amount)
+			} else {
+				fmt.Printf("error: %s\n", err.Error())
+			}
+		}
+	}
+
+	return b
+}
--- gopoker-console/main.go
+++ gopoker-console/main.go
@@ -1,25 +1,23 @@
 package main
 
 import (
-	"bufio"
 	"flag"
 	"fmt"
 	"log"
 	"os"
-	"strconv"
-	"strings"
 )
 
 import (
-	"gopoker/poker"
 	"gopoker/protocol"
-	"gopoker/util/console"
-
+	
 	"gopoker/model"
 	"gopoker/model/game"
 
 	"gopoker/play"
 	"gopoker/play/command"
+
+	"gopoker/client"
+	"gopoker/client/console_client"
 )
 
 var (
@@ -45,109 +43,16 @@ func main() {
 
 	me := make(protocol.MessageChannel)
 	play := createPlay(me)
+	fmt.Printf("%s\n", play)
+
 	go play.Run()
 	play.Control <- command.NextDeal
 
-	fmt.Printf("%s\n", play)
-
-	for {
-		select {
-		case msg := <-me:
-
-			log.Println(console.Color(console.GREEN, fmt.Sprintf("[receive] %s", msg)))
-
-			switch msg.Payload.(type) {
-			case protocol.RequireBet:
-
-				r := msg.Payload.(protocol.RequireBet)
-
-				fmt.Printf("%s\n", r)
-
-				var newBet *model.Bet
-
-				seat := play.Table.Seat(r.Pos)
-
-				for newBet == nil {
-					newBet = readBet(r.Call, r.Call - seat.Bet)
-
-					err := newBet.Validate(seat, r.BetRange)
-					if err != nil {
-						fmt.Println(err.Error())
-						newBet = nil
-					}
-				}
-
-				if newBet != nil {
-					play.Receive <- protocol.NewAddBet(r.Pos, newBet)
-				}
-
-			case protocol.RequireDiscard:
-
-				r := msg.Payload.(protocol.RequireDiscard)
-
-				seat := play.Table.Seat(r.Pos)
-
-				fmt.Printf("Your cards: [%s]\n", play.Deal.Pocket(seat.Player))
-
-				var cards *poker.Cards
-				for cards == nil {
-					cards = readCards()
-				}
-
-				play.Receive <- protocol.NewDiscardCards(r.Pos, cards)
-
-			case protocol.DealCards:
-
-				payload := msg.Payload.(protocol.DealCards)
-
-				if payload.Type.IsBoard() {
-					fmt.Printf("Dealt %s %s\n", payload.Type, payload.Cards.ConsoleString())
-				} else {
-					fmt.Printf("Dealt %s %s to %d\n", payload.Type, payload.Cards.ConsoleString(), payload.Pos)
-				}
-
-			case protocol.MoveButton:
-
-				payload := msg.Payload.(protocol.MoveButton)
-
-				fmt.Printf("Button is %d\n", payload.Pos+1)
-
-			case protocol.AddBet:
-
-				payload := msg.Payload.(protocol.AddBet)
-
-				player := play.Table.Player(payload.Pos)
-
-				fmt.Printf("Player %s: %s\n", player, payload.Bet)
-
-			case protocol.PotSummary:
-
-				payload := msg.Payload.(protocol.PotSummary)
-
-				fmt.Printf("Pot size: %.2f\nBoard: %s\n", payload.Amount, play.Deal.Board.ConsoleString())
-
-			case protocol.ShowHand:
-
-				payload := msg.Payload.(protocol.ShowHand)
-
-				player := play.Table.Player(payload.Pos)
-
-				fmt.Printf("Player %s has %s (%s)\n", player, payload.Cards.ConsoleString(), payload.Hand.HumanString())
-
-			case protocol.Winner:
-
-				payload := msg.Payload.(protocol.Winner)
-
-				fmt.Printf("Player %s won %.2f\n", payload.Player, payload.Amount)
-
-			case protocol.ChangeGame:
-
-				payload := msg.Payload.(protocol.ChangeGame)
-
-				fmt.Printf("Game changed to %s %s\n", payload.Type, payload.Limit)
-			}
-		}
+	conn := &console_client.Connection{
+		Play: play,
 	}
+	session := client.NewSession(conn)
+	session.Start()
 }
 
 func createPlay(me protocol.MessageChannel) *play.Play {
@@ -180,78 +85,4 @@ func createPlay(me protocol.MessageChannel) *play.Play {
 	}
 
 	return play
-}
-
-func readLine() string {
-	fmt.Print(">>> ")
-	str, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-	str = strings.TrimRight(str, "\n")
-	return str
-}
-
-func readBet(call float64, toCall float64) *model.Bet {
-	var b *model.Bet
-
-	for b == nil {
-		b = parseBet(call, toCall, readLine())
-	}
-
-	return b
-}
-
-func readCards() *poker.Cards {
-	var cards *poker.Cards
-	for cards == nil {
-		str := readLine()
-		var err error
-		cards, err = poker.ParseCards(str)
-		if err != nil {
-			fmt.Printf("error: %s\n", err)
-		}
-	}
-	return cards
-}
-
-func parseBet(call float64, toCall float64, str string) *model.Bet {
-	var b *model.Bet
-
-	switch str {
-	case "":
-		if toCall == 0. { // nothing to call
-			b = model.NewCheck()
-		} else {
-			b = model.NewCall(call)
-		}
-
-	case "fold":
-		b = model.NewFold()
-
-	case "check":
-		b = model.NewCheck()
-
-	case "call":
-		b = model.NewCall(call)
-
-	default:
-		parts := strings.Split(str, " ")
-
-		var amountString string
-		if len(parts) == 1 {
-			amountString = parts[0]
-		} else if len(parts) == 2 && parts[0] == "raise" {
-			amountString = parts[1]
-		}
-
-		if amountString != "" {
-			amount, err := strconv.ParseFloat(amountString, 64)
-
-			if err == nil {
-				b = model.NewRaise(amount)
-			} else {
-				fmt.Printf("error: %s\n", err.Error())
-			}
-		}
-	}
-
-	return b
 }
