e37a12cf4b008303645d838027e4fff31abe87b2
Malik Baktiyarov <malikbakt@gmail.com>
2013-07-22 17:07:40 +0400

    minor changes

--- server/rpc_handlers.go
+++ server/rpc_handlers.go
@@ -6,7 +6,6 @@ import (
 
 func (n *NodeRPC) CreateRoom(createRoom *rpc_service.CreateRoom, r *rpc_service.CallResult) error {
 	room := NewRoom(createRoom)
-
 	n.Node.AddRoom(room)
 
 	return nil
@@ -14,7 +13,6 @@ func (n *NodeRPC) CreateRoom(createRoom *rpc_service.CreateRoom, r *rpc_service.
 
 func (n *NodeRPC) DeleteRoom(requestRoom *rpc_service.RequestRoom, r *rpc_service.CallResult) error {
 	room := n.Node.Room(requestRoom.Id)
-
 	n.Node.RemoveRoom(room)
 
 	return nil
@@ -23,16 +21,13 @@ func (n *NodeRPC) DeleteRoom(requestRoom *rpc_service.RequestRoom, r *rpc_servic
 // send protocol message to room subscribers
 func (n *NodeRPC) NotifyRoom(notifyRoom *rpc_service.NotifyRoom, r *rpc_service.CallResult) error {
 	room := n.Node.Room(notifyRoom.Id)
-	msg := notifyRoom.Message
-
-	room.Receive <- msg
+	room.Receive <- notifyRoom.Message
 
 	return nil
 }
 
 func (n *NodeRPC) StartRoom(startRoom *rpc_service.StartRoom, r *rpc_service.CallResult) error {
 	room := n.Node.Room(startRoom.Id)
-
 	room.Start()
 
 	return nil
@@ -40,7 +35,6 @@ func (n *NodeRPC) StartRoom(startRoom *rpc_service.StartRoom, r *rpc_service.Cal
 
 func (n *NodeRPC) PausePlay(requestRoom *rpc_service.RequestRoom, r *rpc_service.CallResult) error {
 	room := n.Node.Room(requestRoom.Id)
-
 	room.Pause()
 
 	return nil
@@ -48,7 +42,6 @@ func (n *NodeRPC) PausePlay(requestRoom *rpc_service.RequestRoom, r *rpc_service
 
 func (n *NodeRPC) ResumePlay(requestRoom *rpc_service.RequestRoom, r *rpc_service.CallResult) error {
 	room := n.Node.Room(requestRoom.Id)
-
 	room.Resume()
 
 	return nil
@@ -56,7 +49,6 @@ func (n *NodeRPC) ResumePlay(requestRoom *rpc_service.RequestRoom, r *rpc_servic
 
 func (n *NodeRPC) CloseRoom(requestRoom *rpc_service.RequestRoom, r *rpc_service.CallResult) error {
 	room := n.Node.Room(requestRoom.Id)
-
 	room.Close()
 
 	return nil
