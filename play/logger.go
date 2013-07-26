22681aad857f583dadcf14c3e51182acdac2c652
Malik Baktiyarov <malik@baktiyarov.com>
2013-07-27 03:22:55 +0400

    play: simple file logger

--- play/logger.go
+++ play/logger.go
@@ -1,7 +1,8 @@
 package play
 
 import (
-	"log"
+	"io"
+	"fmt"
 )
 
 import (
@@ -9,11 +10,13 @@ import (
 )
 
 type Logger struct {
+	Writer io.Writer
 	Recv protocol.MessageChannel
 }
 
-func NewLogger() *Logger {
+func NewLogger(writer io.Writer) *Logger {
 	logger := &Logger{
+		Writer: writer,
 		Recv: make(protocol.MessageChannel),
 	}
 
@@ -25,6 +28,53 @@ func NewLogger() *Logger {
 func (l *Logger) receive() {
 	for {
 		msg := <-l.Recv
-		log.Printf("%s", msg)
+		l.handle(msg)
 	}
+}
+
+func (l *Logger) handle(msg *protocol.Message) {
+	switch msg.Payload().(type) {
+	case protocol.RequireBet:
+	case protocol.RequireDiscard:
+
+	case protocol.DealCards:
+		payload := msg.Envelope.DealCards
+
+		if payload.Type.IsBoard() {
+			l.log("Dealt %s %s\n", payload.Type, payload.Cards.PrintString())
+		} else {
+			l.log("Dealt %s %s to %d\n", payload.Type, payload.Cards.PrintString(), payload.Pos)
+		}
+
+	case protocol.MoveButton:
+		payload := msg.Envelope.MoveButton
+		l.log("Button is %d\n", payload.Pos+1)
+
+	case protocol.AddBet:
+
+		payload := msg.Envelope.AddBet
+		l.log("Payload: %s", payload)
+
+	case protocol.StreetStart:
+
+		payload := msg.Envelope.StreetStart
+		l.log("Payload: %s", payload)
+
+	case protocol.ShowHand:
+
+		payload := msg.Envelope.ShowHand
+		l.log("Payload: %s", payload)
+
+	case protocol.Winner:
+		payload := msg.Envelope.Winner
+		l.log("Payload: %s", payload)
+
+	case protocol.ChangeGame:
+		payload := msg.Envelope.ChangeGame
+		l.log("Payload: %s", payload)
+	}
+}
+
+func (l *Logger) log(format string, args... interface{}) {
+	fmt.Fprintf(l.Writer, format, args...)
 }
