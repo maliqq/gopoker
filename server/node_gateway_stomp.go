8911023680dd64fda580c046672248a189c5d042
Malik Baktiyarov <malik@baktiyarov.com>
2013-09-17 03:46:53 +0400

    stomp queue draft

--- server/node_gateway_stomp.go
+++ server/node_gateway_stomp.go
@@ -1,9 +1,51 @@
 package server
 
 import (
+	"github.com/jjeffery/stomp"
 	stomp_server "github.com/jjeffery/stomp/server"
 )
 
 type NodeStomp struct {
-	server stomp_server.Server
+	*Node
+	server *stomp_server.Server
+}
+
+type NodeStompQueue struct {
+}
+
+func StartStomp(addr string) *NodeStomp {
+	server := stomp_server.Server{
+		Addr: addr,
+		//QueueStorage: NewNodeStompQueue(),
+	}
+	go server.ListenAndServe()
+
+	stomp := NodeStomp{
+		server: &server,
+	}
+
+	return &stomp
+}
+
+func NewNodeStompQueue() *NodeStompQueue {
+	queue := NodeStompQueue{}
+	return &queue
+}
+
+func (q *NodeStompQueue) Start() {
+}
+
+func (q *NodeStompQueue) Stop() {
+}
+
+func (q *NodeStompQueue) Enqueue(queue string, frame *stomp.Frame) error {
+	return nil
+}
+
+func (q *NodeStompQueue) Requeue(queue string, frame *stomp.Frame) error {
+	return nil
+}
+
+func (q *NodeStompQueue) Dequeue(queue string) (*stomp.Frame, error) {
+	return nil, nil
 }
