3623612c3b328037a8f71328ed765aa8ec0579c2
Malik Baktiyarov <malikbakt@gmail.com>
2013-10-25 21:31:49 +0400

    bench upd

--- bench/math_bench.go
+++ bench/math_bench.go
@@ -9,21 +9,37 @@ import (
 
 const (
 	oppNum = 6
+	nano = 1000000000.0
 )
 
 func measure(samplesNum int) {
-	var total int64
+	n := 100
+
+	var total, min, max int64
 	total = 0
-	dealer := model.NewDealer()
-	cards := dealer.Deal(2)
-	board := dealer.Share(3)
-	start := time.Now().UnixNano()
+	min = -1
+	max = 0
 
-	math.ChancesAgainstN{OpponentsNum: oppNum, SamplesNum: samplesNum}.WithBoard(cards, board)
-	t := time.Now().UnixNano() - start
-	total += t
+	for i := 0; i < n; i++ {
+		dealer := model.NewDealer()
+		cards := dealer.Deal(2)
+		board := dealer.Share(3)
+		start := time.Now().UnixNano()
 
-	fmt.Printf("samplesNum=%d time=%.3fs\n", samplesNum, float64(total) / 1000000000.0)
+		math.ChancesAgainstN{OpponentsNum: oppNum, SamplesNum: samplesNum}.WithBoard(cards, board)
+		t := time.Now().UnixNano() - start
+		if (min == -1) { min = t }
+		if (t > max) { max = t }
+		if (t < min) { min = t }
+		total += t
+	}
+
+	fmt.Printf("samplesNum=%d\tn=%d\ttotal=%.3fs\tavg=%.3fs\tmin=%.3fs\tmax=%.3fs\n", 
+		samplesNum, n,
+		float64(total) / nano,
+		float64(total) / 10 / nano,
+		float64(min) / nano,
+		float64(max) / nano)
 }
 
 func main() {
